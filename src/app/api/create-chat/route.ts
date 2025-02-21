c7e07c663f5f7cf903b3825caef3a39fcd65826f
whosensei <sukunagojo965@gmail.com>
2025-02-22 01:07:05 +0530

    react-query setup completed

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -5,10 +5,12 @@ export async function POST (req : Request , res : Response) {
     try {
         const data = await req.json();
         const {file_key, file_name} = data;
+        console.log(file_key, file_name);
+        return NextResponse.json({message : "success at backend"},{status : 200})
 
     }
     catch(error){
         console.error(error);
-        NextResponse.json({error: "Internal Server Error"} ,{status : 500})
+        return NextResponse.json({error: "Internal Server Error"} ,{status : 500})
     }
 }
--- src/components/ui/Fileupload.tsx
+++ src/components/ui/Fileupload.tsx
@@ -1,10 +1,23 @@
 "use client";
 import React from "react"
 import { useDropzone} from "react-dropzone"
-import {Inbox} from "lucide-react"
+import {Inbox, Loader2} from "lucide-react"
 import { uploadToS3 } from "@/lib/db/s3";
+import {useMutation} from "@tanstack/react-query"
+import axios from "axios"
+import {Toaster, toast} from "react-hot-toast"
+import { useState } from "react"
 
 export const Fileupload = () => {
+
+    const [upload,setuploading] = useState(false);
+
+    const { mutate, isPending } = useMutation({
+        mutationFn : async ({file_key,file_name} : {file_key:string,file_name :string})=>{
+            const response = await axios.post("/api/create-chat" , {file_key,file_name});
+            return response.data;
+        },
+    });
 
     // Define accepted file types and dropzone configuration
     const dropzoneOptions = {
@@ -16,16 +29,32 @@ export const Fileupload = () => {
             console.log(acceptedFiles);
             const file = acceptedFiles[0];
             if(file.size >10*1024*1024){
-                alert("please upload a file less than 10mb")
+                toast.error("please upload a file less than 10mb")
                 return
             }
             try{
+                setuploading(true);
                 const data = await uploadToS3(file);
+                if(!data?.file_key || !data.file_name){
+                    toast.error("something went wrong")
+                    return;
+                }
+
+                mutate(data , {
+                    onSuccess: (data) =>{
+                        toast.success(data.message)
+                    },
+                    onError: (error)=>{
+                        toast.error("error creating chat")
+                    }
+                })
                 console.log("File uploaded successfully")
                 console.log("data", data)
             }
             catch(error){
                 console.log(error)
+            }finally{
+                setuploading(false);
             }
         }
     };
@@ -36,10 +65,18 @@ export const Fileupload = () => {
         <div className="bg-slate-100 rounded-xl p-2">
             <div {...getRootProps()} className= " bg-slate-100 border-slate-400 border-dashed border-2 rounded-xl flex justify-center items-center flex-col cursor-pointer">
                 <input {...getInputProps()} />
+                {(upload || isPending) ? (
+                    <>
+                    <Loader2 className = "w-12 h-12 p-2 color-slate-200 text-slate-500 mt-2 animate-spin" />
+                    <p className = "mt-2 text-slate-400">
+                        uploading...
+                    </p>
+                    </>
+                ) :(
                 <>
                 <Inbox className = " w-12 h-12 p-2 color-slate-200 text-slate-500 mt-2" />
                 <div className = "px-2 ml-2 mr-2 mb-2 text-slate-500">Drop Your File here</div>
-                </>
+                </>)}
             </div>
         </div>
     )
