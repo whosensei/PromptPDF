f3ee62bcd32e3de255afff7f74ed8f876aa678a7
whosensei <sukunagojo965@gmail.com>
2025-04-30 16:09:57 +0530

    Used the latest gemini api

--- src/lib/db/embeddings.ts
+++ src/lib/db/embeddings.ts
@@ -4,10 +4,13 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function getEmbeddings(text: string) {
     try {
+        text = text.replace(/\n/g, " ")
         const result = await ai.models.embedContent({
-            model: "text-embedding-004",
-            contents: text.replace(/\n/g, " ") ,
-            config: { outputDimensionality: 10 },
+            model: 'gemini-embedding-exp-03-07',
+            contents: 'What is the meaning of life?',
+            config: {
+                taskType: "RETRIEVAL_DOCUMENT, RETRIEVAL_QUERY, QUESTION_ANSWERING, and FACT_VERIFICATION",
+            }
           });
           console.log(result.embeddings);
           console.log(result.embeddings?.values);
